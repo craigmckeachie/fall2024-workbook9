96c15e5e42911295c44072055716f149871b5ac0
Craig McKeachie <craigdmckeachie@gmail.com>
2024-12-11 10:24:37 -0500

    add method

--- demos/object-oriented/first-class.js
+++ demos/object-oriented/first-class.js
@@ -1,17 +1,21 @@
 "use strict";
 
 class Person {
-
   constructor(first, last) {
     this.first = first;
     this.last = last;
   }
-  
+
+  displayFullName() {
+    console.log(this.first, this.last);
+  }
 }
 
 let person = new Person("Freddie", "Gibbs");
-
 let anotherPerson = new Person("Fred", "Flintstone");
 
 console.log(person);
 console.log(anotherPerson);
+
+person.displayFullName();
+anotherPerson.displayFullName();
